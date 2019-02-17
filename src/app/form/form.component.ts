dd01a48f07be2b968be1d9c8d816490ca21e6927
BaiToeyyi <wilaiwan.a@ku.th>
2019-02-17 11:23:21 +0700

    workshop3

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form.component.less']
 })
 export class FormComponent implements OnInit {
-
-  constructor() { }
+  formGroup: FormGroup;
+  constructor(
+    private formBulid: FormBuilder
+  ) { 
+    
+  }
 
   ngOnInit() {
+    this.formGroup = this.formBulid.group({
+      firstName: [''],
+      lastName: ['']
+    })
   }
 
 }
